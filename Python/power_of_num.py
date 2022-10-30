8af7dac567a05848bc44eab8602ffa98e4958993
quala-laos <116997144+quala-laos@users.noreply.github.com>
2022-10-30 11:53:33 +0530

    Create power_of_num.py

---
+++ Python/power_of_num.py
@@ -0,0 +1,9 @@
+# python program to find the power of a number
+
+a = 10
+b = 3
+
+# calculating power using exponential oprator (**)
+result = a**b
+
+print (a, " to the power of ", b, " is = ", result)
